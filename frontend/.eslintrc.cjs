513ad34a7c073d9ec0ee5bc5926882b1d44bd3d3
itzRoy <royissa3@gmail.com>
2023-07-28 02:24:29 +0300

    setup

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "semi": ["error", "always"],
+    "no-console": "error",
+    "no-multiple-empty-lines": ["error", { "max": 3, "maxEOF": 0, "maxBOF": 1 }],
+    "comma-dangle": ["error", "never"],
+    // or
+    "comma-dangle": ["error", {
+        "arrays": "always-multiline",
+        "objects": "always-multiline",
+        "imports": "always-multiline",
+        "exports": "always-multiline",
+    }],
+    "array-element-newline": ["error", { "multiline": true, "minItems": 3 }],
+    "object-curly-newline": ["error", {
+      "ObjectExpression": "always",
+      "ObjectPattern": { "multiline": true },
+      "ImportDeclaration": "never",
+      "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+  }],
+    "object-curly-spacing": ["error", "always"],
+    "key-spacing": ["error", { "beforeColon": false }],
+    "padded-blocks": ["error", "always"],
+    "newline-after-var": ["error", "always"],
+    "object-property-newline": "error",
+    "linebreak-style": ["error", "unix"],
+    "padding-line-between-statements": ["error", { blankLine: "always", prev: ["const", "let", "var", "import", "function", "block-like", "expression"], next: ["function", "block-like", "expression", "export"]}]
+  },
+}
---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,11 @@
+import hideIcon from './No-Eyehide.svg';
+import showIcon from './No-Eyeshow.svg';
+import profileIcon from './photoprofile.svg';
+import themeIcon from './Grouptheme.svg';
+
+export default {
+    hideIcon,
+    showIcon,
+    profileIcon,
+    themeIcon,
+};
