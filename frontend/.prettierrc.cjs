dd0853c88b05ec952ee389b71b44a35b464a8fc8
itzRoy <royissa3@gmail.com>
2023-07-28 02:24:29 +0300

    setup

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 120,
+    proseWrap: "always",
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    semi: true,
+    jsxSingleQuote: false,
+    endOfLine: "lf"
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        text: '#F4F6FA',
+        input: '#2E3239',
+        background: '#23262C',
+      },
+},
+},
+  plugins: [],
+};
