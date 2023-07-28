c8b9c5367368887d856497fd496fc6ae53ed50fc
itzRoy <royissa3@gmail.com>
2023-07-28 18:51:00 +0300

    ``

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('./dbConnect');
 const express = require('express');
 const authMiddleware = require('./src/middlware/authentication');
 const config = require('./config.js');
+const cors = require('cors');
 
 const userRoutes = require('./src/user/routes');
 const todoRoutes = require('./src/todo/routes');
@@ -9,6 +10,11 @@ const todoRoutes = require('./src/todo/routes');
 const PORT = config.port || 5000;
 
 const app = new express();
+
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
