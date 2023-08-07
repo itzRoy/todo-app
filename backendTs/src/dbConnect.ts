e55fb1571a24ce6c95190572850dafdcc8ed61c8
itzRoy <royissa3@gmail.com>
2023-08-07 20:57:43 +0300

    resolve graphql version

--- backendTs/src/dbConnect.ts
+++ backendTs/src/dbConnect.ts
@@ -2,4 +2,4 @@
 import mongoose from 'mongoose';
 import config from './config.js';
 
-export default () => mongoose.connect(config.db || '', {dbName: 'todoDB'});
+export default () => mongoose.connect(config.db || '', {dbName: 'todoDB'}).then(() => console.log('connected to mongoDB')).catch(e => e);
